889e2d1af8f421287fde4d6874c58030d5ec5ac1
Daniel Noguchi <danielnoguchi@gmail.com>
2015-02-25 15:02:28 -0800

    Tinker with single linked list

--- lru/single_linked_list.py
+++ lru/single_linked_list.py
@@ -2,6 +2,8 @@
 Simple implementation of a singly linked list
 (based on http://ls.pwd.io/2014/08/singly-and-doubly-linked-lists-in-python/)
 """
+import copy
+
 class Node(object):
     """
     Node base class
@@ -18,30 +20,35 @@ class SingleLinkedList(object):
     head = None
     tail = None
 
-    def append(self, value):
+    def append(self, node):
         # Check if list is empty
-        new_node = Node(value, None)
         if self.head is None:
-            self.head = self.tail =  new_node
+            self.head = self.tail =  node
         else:
-            self.tail.next = new_node
-        self.tail = new_node
+            self.tail.next = node
+        self.tail = node
 
-    def remove(self, value):
+    def remove(self, node):
         prev_node = self.head
         curr_node = self.head.next
-        while curr_node.next is not None:
-            if curr_node.value == value:
+        while prev_node is not None:
+            if curr_node.next == node.next:
                 if curr_node.next is not None:
                     prev_node.next = curr_node.next
                 else:
-                    self.head = curr_node.next
+"single_linked_list.py" 76L, 1866C
+            curr_node = curr_node.next
             # Is this correct?
             # curr_node = curr_node.next
             # prev_node = prev_node.next
+
+
+    def append(self, node):$
             prev_node = curr_node
             curr_node = curr_node.next
-                
+        return node
+
+
     def show(self):
         node = self.head
         while node.next is not None:
@@ -49,3 +56,25 @@ class SingleLinkedList(object):
             node = node.next
         print None
 
+    # Exercise: Given the pointer/reference to the head of a singly linked list,
+    # reverse it and return the pointer/reference to the head of the reversed
+    # linked list
+    def reverse(self, single_list):
+        rev = SingleLinkedList()
+        li = copy.copy(single_list)
+        while li is not None:
+            node = li.remove(li.tail)
+            rev.append(node)
+
+s = SingleLinkedList()
+s.append(31)
+s.append(2)
+s.append(3)
+s.append(4)
+
+
+s.show()
+s.remove(31)
+s.remove(3)
+s.remove(2)
+s.show()
