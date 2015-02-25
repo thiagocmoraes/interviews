1de9f448642c00f2fd26001ce86fd6aa6b6a6efc
Daniel Noguchi <danielnoguchi@gmail.com>
2015-02-25 15:07:44 -0800

    Recover file

--- lru/single_linked_list.py
+++ lru/single_linked_list.py
@@ -2,8 +2,6 @@
 Simple implementation of a singly linked list
 (based on http://ls.pwd.io/2014/08/singly-and-doubly-linked-lists-in-python/)
 """
-import copy
-
 class Node(object):
     """
     Node base class
@@ -20,22 +18,20 @@ class SingleLinkedList(object):
     head = None
     tail = None
 
-    def append(self, node):
+    def append(self, value):
         # Check if list is empty
+        new_node = Node(value, None)
         if self.head is None:
-            self.head = self.tail =  new
-
-
-                    self.head = curr_node.next
+            self.head = self.tail =  new_node
         else:
-            self.tail.next = new
-        self.tail = new
+            self.tail.next = new_node
+        self.tail = new_node
 
-    def remove(self, node):
+    def remove(self, value):
         prev_node = self.head
         curr_node = self.head.next
-        while prev_node is not None:
-            if curr_node.next == node.next:
+        while curr_node.next is not None:
+            if curr_node.value == value:
                 if curr_node.next is not None:
                     prev_node.next = curr_node.next
                 else:
@@ -45,21 +41,11 @@ class SingleLinkedList(object):
             # prev_node = prev_node.next
             prev_node = curr_node
             curr_node = curr_node.next
-        return node
-
-    # Exercise: Given the pointer/reference to the head of a singly linked list,
-    # reverse it and return the pointer/reference to the head of the reversed
-    # linked list
-    def reverse(self, single_list):
-        rev = SingleLinkedList()
-        li = copy.copy(single_list)
-        while li is not None:
-            node = li.remove(li.tail)
-            rev.append(node)
-
+                
     def show(self):
         node = self.head
         while node.next is not None:
             print node.value,
             node = node.next
         print None
+
