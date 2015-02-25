d73e705d9faa20d9ab148e9af3303816c99542dc
Daniel Noguchi <danielnoguchi@gmail.com>
2015-02-25 15:05:45 -0800

    Recover file

--- lru/single_linked_list.py
+++ lru/single_linked_list.py
@@ -23,10 +23,13 @@ class SingleLinkedList(object):
     def append(self, node):
         # Check if list is empty
         if self.head is None:
-            self.head = self.tail =  node
+            self.head = self.tail =  new
+
+
+                    self.head = curr_node.next
         else:
-            self.tail.next = node
-        self.tail = node
+            self.tail.next = new
+        self.tail = new
 
     def remove(self, node):
         prev_node = self.head
@@ -36,25 +39,13 @@ class SingleLinkedList(object):
                 if curr_node.next is not None:
                     prev_node.next = curr_node.next
                 else:
-"single_linked_list.py" 76L, 1866C
-            curr_node = curr_node.next
+                    self.head = curr_node.next
             # Is this correct?
             # curr_node = curr_node.next
             # prev_node = prev_node.next
-
-
-    def append(self, node):$
             prev_node = curr_node
             curr_node = curr_node.next
         return node
-
-
-    def show(self):
-        node = self.head
-        while node.next is not None:
-            print node.value,
-            node = node.next
-        print None
 
     # Exercise: Given the pointer/reference to the head of a singly linked list,
     # reverse it and return the pointer/reference to the head of the reversed
@@ -66,15 +57,9 @@ class SingleLinkedList(object):
             node = li.remove(li.tail)
             rev.append(node)
 
-s = SingleLinkedList()
-s.append(31)
-s.append(2)
-s.append(3)
-s.append(4)
-
-
-s.show()
-s.remove(31)
-s.remove(3)
-s.remove(2)
-s.show()
+    def show(self):
+        node = self.head
+        while node.next is not None:
+            print node.value,
+            node = node.next
+        print None
