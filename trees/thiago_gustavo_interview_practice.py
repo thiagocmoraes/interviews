6816a9891967b72219b82da5a2fa059c420dac6e
Daniel Noguchi <danielnoguchi@gmail.com>
2015-02-25 18:42:02 -0800

    Add final code from the rehearsal

---
+++ trees/thiago_gustavo_interview_practice.py
@@ -0,0 +1,73 @@
+# Given an increasingly sorted array and a number s, please find two numbers whose sum is s. If there are multiple pairs with sum s, just output any one of them.
+
+# For example, if the inputs are an array {1, 2, 4, 7, 11, 15} and a number 15, please out two numbers 4 and 11 since 4+11=15.
+
+# Iteratively find substets from given array and add them up as we go and compare with the given value
+
+def do_they_sum(arr, num):
+    return v1, v2 # v1+v2 == num
+
+def summed_pair(arr):
+    # The last number to check is going to be always smaller than num
+    start = 0
+    end = len(arr)-1
+
+    while arr[end] >= num:
+        end -= 1
+
+    j = 0
+    for i in arr:
+        while j <= end:
+            aux = arr[i] + arr[j]
+            if aux == num:
+                return arr[i], arr[j]
+            j += 1
+    return None
+
+#thiago's solution
+a = [1, 3, 4, 5, 6, 7, 11, 15]
+def has_two_numbers_sum(a, n):
+    if len(a) <= 1:
+        return None
+
+    small = 0
+    big = len(a)-1
+
+    while arr[big] >= num:
+        big -= 1
+    while small < big:
+        currSum = a[small] + a[big]
+        if currSum == n:
+            return a[small], a[big]
+        else:
+            if currSum > n:
+                big -= 1
+            else:
+                small += 1
+    return None
+
+#part 1
+print('part 1')
+print(has_two_numbers_sum(a, 15))
+print(has_two_numbers_sum(a, 19))
+print(has_two_numbers_sum(a, 1))
+print(has_two_numbers_sum(a, 6))
+
+# Given an array, please determine whether it contains three numbers whose sum equals to 0.
+def has_sum_zero(arr):
+    for i in range(len(arr)):
+        a = -arr[i]
+        for j in arr[i+1:]:
+            t = has_two_numers(arr[i+1:], j)
+            if t:
+                return a, t[0], t[1]
+
+
+# Check if a substring is cointained within a given string
+def strstr(needle, haystack):
+    found = False
+    for i in range(len(haystack)):
+        aux = haystack[i:len(needle)]
+        if aux == needle
+            return True
+
