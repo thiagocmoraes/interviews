49e3aaacf3ee3197cc3a6d268a3cd501daf34230
Thiago Moraes <thiago@cmoraes.com>
2015-02-26 16:42:00 -0300

    Update permutations.py

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -25,6 +25,32 @@ def permutations2(word):
 # Using python awesomeness
 def permutations3(word):
     return itertools.permutations(word)
+    
+# thiago's solution
+def permute(lst, n):
+    # you yield tuples of values for permutations of lst elements in groups of n elements 
+    for i in range(len(lst)):
+        if n == 1:
+            yield (lst[i],) #returns a tuple with the element
+        else:
+            # get a permute of all elements EXCEPT the one you're at, taken in smaller groups (n-1)
+            for elem in permute(lst[:i] + lst[i+1:], n-1): 
+                # for each element of this smaller permutation, you add you current element in front of it
+                yield (lst[i],) + elem
+                
+def permute_trace(lst, n):
+    for i in range(len(lst)):
+        if n == 1:
+            print 'base', (lst[i],)
+            yield (lst[i],)
+        else:
+            for elem in permute(lst[:i] + lst[i+1:], n-1):
+                print 'list + elem', (lst[i],) + elem
+                yield (lst[i],) + elem
+            print
+
+print 'thiago solution', list(permute('abc', 3))
+print 'follow the trace\n', list(permute_trace('abc', 3))
 
 
 # How to use it:
